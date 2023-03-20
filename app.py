3b1083694fc20e2aa50064c8466727a5943290ae
ASHRITHSHETTY123 <ashrithshetty14@gmail.com>
2023-03-20 14:16:27 +0530

    sdsdaddsdsds s da  sd sd ds ds ds ds d

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ repo = Repo(r"C:\Users\ASHRITH SHETTY\Desktop\Notepad_Tracker")
 
 @app.route('/')
 def index():
-    
-    return render_template('index.html')
+    os.chdir(r"C:\Users\ASHRITH SHETTY\Desktop\Notepad_Tracker\docs")
+    file= glob.glob("*.txt")
+    return render_template('index.html', text_file=file)
 
 @app.route('/save', methods=['POST'])
 def save():
